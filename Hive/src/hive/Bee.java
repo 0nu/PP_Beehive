dc7e99e6481f415370482b586299c3f4eaadf4d7
t <t@t-X75A1>
2014-09-22 20:00:42 +0200

    dd

--- Hive/src/hive/Bee.java
+++ Hive/src/hive/Bee.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 /**
  * The bee - lots of information is stored here. Each bee object is meant to be
  * run as single thread, as there are lots of actions, interactions, decisions
- * and death (not implemented yet).
+ * and death (not implemented yet).dcvd
  * 
  * @author ole
  * 
